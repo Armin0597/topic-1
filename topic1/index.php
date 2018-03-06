509d387d0b4181bf0f5743fbe295adb915195e20
Armin0597 <dika0597@gmail.com>
2018-03-07 00:02:05 +0700

    Add files via upload

---
+++ topic1/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Make Different</title>
+        <link href="https://fonts.googleapis.com/css?family=Pacifico" 
+              rel="stylesheet">
+    </head>
+    <body >
+        <h1 >this is heading</h1>
+        <p>this is paragraph</p>
+        <h1><i>this</i> heading with <i>italic</i></h1>
+        <h1><u>this</u> heading with <u>underline</u></h1>
+        <p><strike>this is paragraph</strike> has been scratch</p>
+        <article>
+            <h1 style="color:#001433">this is made with tag article</h1>
+            <font face="pacifico">lorem ipsum qwerty.</font>
+        </article>
+        <p><strong>Note:</strong> still trying to learn html more.</p>
+        <font face="pacifico" size="10"> make with tag font</font> 
+        <br />
+        <a href="https://www.w3schools.com">Visit W3Schools.com!</a>
+        <br />
+        <font size="10">how to learn <acronym title="more fast">HTML?</acronym></font>
+        <br />
+        <img src="https://www.gettyimages.com/license/611460021" alt="diving">
+    </body>
+</html>
